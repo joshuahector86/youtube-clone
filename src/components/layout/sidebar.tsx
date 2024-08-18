410386a54f644c8ba82a97968d0dd7bb29d3bbd1
Dimitri Hector <joshuahector86@gmail.com>
2024-08-18 18:44:51 -0400

    adding zustand store to toggle sidebar

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -9,100 +9,110 @@ import {
 import { ChevronRight } from "lucide-react";
 import { Link } from "react-router-dom";
 import Copyright from "../one-offs/copyright";
+import { useSidebarDisplayStore } from "@/stores/sidebar-display-store";
 
 const Sidebar = () => {
   const isHome = (linkName: string): boolean => {
     return linkName === "Home";
   };
+  const { isDisplay } = useSidebarDisplayStore();
   return (
-    <div className="p-2 pb-20 flex flex-col gap-1 overflow-y-auto min-w-[16rem]">
-      {/* FIRST SET OF URLS */}
-      {sidebarUrls1.map((link) => (
-        <Link
-          key={link.name}
-          to={link.href}
-          className={`rounded flex gap-6 items-center py-2 px-4 hover:bg-[#717171] hover:cursor-default 
+    <div>
+      {isDisplay ? (
+        <div className="p-2 pb-20 flex flex-col gap-1 overflow-y-auto min-w-[16rem]">
+          {/* FIRST SET OF URLS */}
+          {sidebarUrls1.map((link) => (
+            <Link
+              key={link.name}
+              to={link.href}
+              className={`rounded flex gap-6 items-center py-2 px-4 hover:bg-[#717171] hover:cursor-default 
              w-full  ${isHome(link.name) ? "bg-[#717171]" : ""}`}
-        >
-          <img src={link.icon} alt="icon-for-link" className="w-6" />
-          {link.name}
-        </Link>
-      ))}
-      <div className="w-full  border-muted-foreground border-b " />
-      {/* SECOND SET OF URLS */}
-      <h1 className=" text-md p-2 flex items-center gap-2">
-        You
-        <ChevronRight />
-      </h1>
-      {sidebarUrls2.map((link) => (
-        <Link
-          key={link.name}
-          to={link.href}
-          className={` hover:bg-[#717171] hover:cursor-default rounded flex gap-6 items-center py-2 px-4  
+            >
+              <img src={link.icon} alt="icon-for-link" className="w-6" />
+              {link.name}
+            </Link>
+          ))}
+          <div className="w-full  border-muted-foreground border-b " />
+          {/* SECOND SET OF URLS */}
+          <h1 className=" text-md p-2 flex items-center gap-2">
+            You
+            <ChevronRight />
+          </h1>
+          {sidebarUrls2.map((link) => (
+            <Link
+              key={link.name}
+              to={link.href}
+              className={` hover:bg-[#717171] hover:cursor-default rounded flex gap-6 items-center py-2 px-4  
            w-full `}
-        >
-          <img src={link.icon} alt="icon-for-link" className="w-6" />
-          {link.name}
-        </Link>
-      ))}
-      <div className="w-full  border-muted-foreground border-b " />
-      {/* SUBSCRIPTIONS */}
-      <h1 className=" text-md p-2 flex items-center gap-2">Subscriptions</h1>
-      {subscribedChannels.map((channel) => (
-        <div className="hover:bg-[#717171] hover:cursor-default flex px-4 gap-6 items-center p-2">
-          <img
-            key={channel.name}
-            src={channel.photo}
-            className="flex-shrink-0 w-8 h-8 rounded-full"
-          />
-          <p className="truncate">{channel.name}</p>
+            >
+              <img src={link.icon} alt="icon-for-link" className="w-6" />
+              {link.name}
+            </Link>
+          ))}
+          <div className="w-full  border-muted-foreground border-b " />
+          {/* SUBSCRIPTIONS */}
+          <h1 className=" text-md p-2 flex items-center gap-2">
+            Subscriptions
+          </h1>
+          {subscribedChannels.map((channel) => (
+            <div className="hover:bg-[#717171] hover:cursor-default flex px-4 gap-6 items-center p-2">
+              <img
+                key={channel.name}
+                src={channel.photo}
+                className="flex-shrink-0 w-8 h-8 rounded-full"
+              />
+              <p className="truncate">{channel.name}</p>
+            </div>
+          ))}
+          <div className="w-full  border-muted-foreground border-b " />
+          {/* EXPLORE */}
+          <h1 className=" text-md p-2 flex items-center gap-2">Explore</h1>
+          {sidebarUrls3.map((link) => (
+            <Link
+              key={link.name}
+              to={link.href}
+              className={`hover:bg-[#717171] hover:cursor-default rounded flex gap-6 items-center py-2 px-4  
+           w-full `}
+            >
+              <img src={link.icon} alt="icon-for-link" className="w-6" />
+              {link.name}
+            </Link>
+          ))}
+          <div className="w-full  border-muted-foreground border-b " />
+          {/* MORE FROM CLONETUBE */}
+          <h1 className=" text-md p-2 flex items-center gap-2">
+            More from CloneTube
+          </h1>
+          {sidebarUrls4.map((link) => (
+            <Link
+              key={link.name}
+              to={link.href}
+              className={`hover:bg-[#717171] hover:cursor-default rounded flex gap-6 items-center py-2 px-4  
+           w-full `}
+            >
+              <img src={link.icon} alt="icon-for-link" className="w-6" />
+              {link.name}
+            </Link>
+          ))}
+          <div className="w-full  border-muted-foreground border-b " />
+          {/* SETTINGS */}
+          {sidebarUrls5.map((link) => (
+            <Link
+              key={link.name}
+              to={link.href}
+              className={`hover:bg-[#717171] hover:cursor-default rounded flex gap-6 items-center py-2 px-4  
+           w-full `}
+            >
+              <img src={link.icon} alt="icon-for-link" className="w-6" />
+              {link.name}
+            </Link>
+          ))}
+          <div className="w-full  border-muted-foreground border-b " />
+          <Copyright />
         </div>
-      ))}
-      <div className="w-full  border-muted-foreground border-b " />
-      {/* EXPLORE */}
-      <h1 className=" text-md p-2 flex items-center gap-2">Explore</h1>
-      {sidebarUrls3.map((link) => (
-        <Link
-          key={link.name}
-          to={link.href}
-          className={`hover:bg-[#717171] hover:cursor-default rounded flex gap-6 items-center py-2 px-4  
-           w-full `}
-        >
-          <img src={link.icon} alt="icon-for-link" className="w-6" />
-          {link.name}
-        </Link>
-      ))}
-      <div className="w-full  border-muted-foreground border-b " />
-      {/* MORE FROM CLONETUBE */}
-      <h1 className=" text-md p-2 flex items-center gap-2">
-        More from CloneTube
-      </h1>
-      {sidebarUrls4.map((link) => (
-        <Link
-          key={link.name}
-          to={link.href}
-          className={`hover:bg-[#717171] hover:cursor-default rounded flex gap-6 items-center py-2 px-4  
-           w-full `}
-        >
-          <img src={link.icon} alt="icon-for-link" className="w-6" />
-          {link.name}
-        </Link>
-      ))}
-      <div className="w-full  border-muted-foreground border-b " />
-      {/* SETTINGS */}
-      {sidebarUrls5.map((link) => (
-        <Link
-          key={link.name}
-          to={link.href}
-          className={`hover:bg-[#717171] hover:cursor-default rounded flex gap-6 items-center py-2 px-4  
-           w-full `}
-        >
-          <img src={link.icon} alt="icon-for-link" className="w-6" />
-          {link.name}
-        </Link>
-      ))}
-      <div className="w-full  border-muted-foreground border-b " />
-      <Copyright />
+      ) : (
+        ""
+      )}
     </div>
   );
 };
--- src/components/layout/topbar.tsx
+++ src/components/layout/topbar.tsx
@@ -1,4 +1,3 @@
-import { Input } from "@/components/ui/input";
 import {
   Bell,
   CircleUser,
@@ -6,26 +5,38 @@ import {
   MicIcon,
   Plus,
   SearchIcon,
-  Youtube,
 } from "lucide-react";
+import youtubeIcon from "../../assets/icons/youtubeicon.jpg";
+import { useSidebarDisplayStore } from "@/stores/sidebar-display-store";
+import { Button } from "../ui/button";
 
 const Topbar = () => {
+  const { isDisplay, setIsDisplay } = useSidebarDisplayStore();
+
   return (
     <div className="flex px-4 py-2 justify-between items-center">
       <div className="px-2 flex gap-6 items-center">
         <div>
-          <Menu />
+          <Button
+            variant={"ghost"}
+            className="hover:bg-[#717171]/20 -ml-2 p-2 rounded-full"
+            onClick={() => {
+              setIsDisplay(!isDisplay);
+            }}
+          >
+            <Menu />
+          </Button>
         </div>
         <div className="flex gap-1 items-center">
-          <Youtube color="red" size={40} />
-          CloneTube
+          <img src={youtubeIcon} className="w-10 h-10" alt="youtube-icon" />
+          <p className="font-semibold text-2xl">CloneTube</p>
         </div>
       </div>
       <SearchBar />
-      <div className="flex gap-4">
-        <Plus />
-        <Bell />
-        <CircleUser />
+      <div className="flex gap-8">
+        <Plus className="hover:cursor-default hover:text-[#717171]" />
+        <Bell className="hover:cursor-default hover:text-[#717171]" />
+        <CircleUser className="hover:cursor-default hover:text-[#717171]" />
       </div>
     </div>
   );
@@ -35,8 +46,11 @@ const SearchBar = () => {
   return (
     <div className="flex gap-2 items-center">
       <div className="border rounded-full flex items-center overflow-hidden">
-        <Input className="border-r w-[40rem]" placeholder="Search..." />
-        <SearchIcon className="bg-[#2d2c2c] w-10 h-10 p-2" />
+        <input
+          className="bg-transparent outline-none px-4 w-full lg:w-[40rem] "
+          placeholder="Search..."
+        />
+        <SearchIcon className="bg-[#2d2c2c] w-10 h-10 border-l ml-2 p-2" />
       </div>
       <MicIcon className="bg-[#2d2c2c] rounded-full w-10 h-10 p-2" />
     </div>
